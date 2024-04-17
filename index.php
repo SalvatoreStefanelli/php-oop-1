5257553c3b3eb781bf7ce2fe287125193f1fc444
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-04-17 20:10:30 +0200

    add construct and method

--- index.php
+++ index.php
@@ -20,4 +20,31 @@ class Movie
      }
 }
 
-$first_movie = new Movie('Step Up', 73, 2006);
+class Duration {
+    public $hour;
+    public $word;
+    public $minutes;
+    public $word_2;
+
+    public function __construct($hour, $word, $minutes, $word_2) {
+        $this->hour = $hour;
+        $this->word = $word;
+        $this->minutes = $minutes;
+        $this->word_2 = $word_2;
+    }
+
+    public function getFullDuration() {
+        return $this->hour . ' ' . $this->word . ' ' . $this->minutes . ' ' . $this->word_2;
+    }
+    
+}
+
+$duration_1= new Duration(1, 'ora', 13, 'minuti'); 
+$duration_2= new Duration(2, 'ore', 05, 'minuti'); 
+var_dump($duration_1, $duration_2);
+
+$step_up = new Movie('Step Up', $duration_1, 2006);
+$creed = new Movie('Creed', $duration_2, 2013);
+
+var_dump($step_up->duration->getFullDuration());
+var_dump($creed->duration->getFullDuration());
