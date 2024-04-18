2afa2972576247d1514d54a0a802d09c2e37e848
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-04-18 11:59:20 +0200

    create layout

--- index.php
+++ index.php
@@ -48,3 +48,45 @@ $creed = new Movie('Creed', $duration_2, 2013);
 
 var_dump($step_up->duration->getFullDuration());
 var_dump($creed->duration->getFullDuration());
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+
+    <header class="bg-dark text-white ">
+        <nav class="nav justify-content-center ">
+            <a class="nav-link active" href="#" aria-current="page">
+                Active link
+            </a>
+
+            <a class="nav-link" href="#">
+                Link
+            </a>
+
+            <a class="nav-link disabled" href="#">
+                Disabled link
+            </a>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <div class="container">
+                <div class="row row-cols-1 row-cols-sm-3">
+                    <div class="col"></div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+    
+</body>
+</html>
